ae68c2d4459fa251aa23ee2b1eb4e44cc7604775
ScottyB <scott.n.barnett@gmail.com>
2013-01-30 21:13:49 +1100

    Removed data-root.

--- ac-js2.el
+++ ac-js2.el
@@ -61,11 +61,8 @@
 (defvar js2ac-skewer-candidates '()
   "Cadidates obtained from skewering")
 
-(defvar js2ac-data-root (file-name-directory load-file-name)
-  "Location of data files needed for js2ac-on-skewer-load")
-
 (defun js2ac-on-skewer-load ()
-  (insert-file-contents (expand-file-name "skewer-addon.js" js2ac-data-root)))
+  (insert-file-contents (expand-file-name "skewer-addon.js")))
 
 (add-hook 'skewer-js-hook 'js2ac-on-skewer-load)
 
